ebb6bc907269b1e082157dab0b14980ca2cd33d9
trdoan <tranduongdoan0922@gmail.com>
2022-07-04 15:14:34 +0700

    backup code 0407

---
+++ app/controllers/movie.controller.js
@@ -0,0 +1,90 @@
+const createHttpError = require("http-errors");
+const { uploadImage } = require("../middlewares/upload/upload.middlware");
+const { Movie } = require("./../models");
+const queryString = require("query-string");
+
+const getInfoMovie = async (req, res, next) => {
+  const { id } = req.params;
+  const movie = await Movie.findByPk(id);
+  res.send(movie);
+};
+const getMovieList = async (req, res) => {
+  const _page = +req.query._page || 1;
+  const _item = +req.query._item || 10;
+  const test = await Movie.findAndCountAll({
+    limit: _item,
+    offset: (_page - 1) * _item,
+  });
+
+  res.send({
+    pagination: {
+      totalPage: Math.ceil(test.count / _item),
+      item: _item,
+      page: _page,
+    },
+    movieList: test.rows,
+  });
+  // res.send({
+  //   row,
+  //   pagination: {
+  //     totalPage,
+  //     _page,
+  //   },
+  // });
+};
+const createMovie = async (req, res) => {
+  const {
+    title,
+    description,
+    director,
+    actor,
+    rate,
+    trailer,
+    time,
+    startTime,
+  } = req.body;
+  const { file } = req;
+  const cloud = await uploadImage(file, "poster");
+  const movie = await Movie.create({ ...req.body, poster: cloud.url });
+  res.send(movie);
+};
+const updateMovie = async (req, res, next) => {
+  const { file } = req;
+  const { id } = req.params;
+  try {
+    if (req.file) {
+      const { url } = await uploadImage(req.file, "poster");
+      await Movie.update(
+        { ...req.body, poster: url },
+        {
+          where: { id },
+        }
+      );
+    } else {
+      await Movie.update(req.body, {
+        where: { id },
+      });
+    }
+    res
+      .status(200)
+      .send({ statusCode: 200, message: "Cập nhật phim thành công" });
+  } catch (error) {
+    console.log(error);
+    next(createHttpError.InternalServerError(error));
+  }
+};
+const deleteMovie = async (req, res) => {
+  const { id } = req.params;
+  await Movie.destroy({ where: { id } });
+  res.status(200).send({
+    statusCode: 200,
+    message: "Xoá phim dùng thành công",
+  });
+};
+module.exports = {
+  getInfoMovie,
+  getMovieList,
+  createMovie,
+  updateMovie,
+  deleteMovie,
+};
